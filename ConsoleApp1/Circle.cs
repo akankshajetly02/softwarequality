995ae4aa59645122385d3de18f2911dd7da71e64
Akanksha Jetly <akankshajetly@yahoo.com>
2023-02-15 01:17:34 -0500

    completed ciecle class

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Circle
+    {
+        private int radius;
+        Circle(int radius)
+        {
+            this.radius = 1;
+        }
+    }
+}
